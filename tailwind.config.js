06a286c8acf21fa219d211953073bafa32df71bc
alejo <alejo@undergroundcellar.com>
2021-03-10 20:48:34 -0500

    Init files creating index.html

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'dosis': ['Dosis'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
